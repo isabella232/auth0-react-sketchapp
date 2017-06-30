4216dc0b304ffe8323ffbaa4d487b34a2dc42f20
Ariel Gerstein <ariel.ger@hotmail.com>
2017-06-30 12:54:27 -0300

    Add styled components and refactor Label component

--- examples/styleguide/src/components/Label.js
+++ examples/styleguide/src/components/Label.js
@@ -1,5 +1,6 @@
 import React, { Element } from 'react';
 import chromajs from 'chroma-js';
+import styled from 'styled-components/primitives';
 import { colors, spacing } from '../designSystem';
 import { View, Text, StyleSheet } from 'react-sketchapp';
 
@@ -8,56 +9,43 @@ import { View, Text, StyleSheet } from 'react-sketchapp';
 // Revise sizing of fonts / labels
 // Revise types of labels
 
-const styles = {
-  label: {
-    borderRadius: 3,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    paddingVertical: 3,
-    paddingHorizontal: 12,
-    marginBottom: spacing,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  default: {
-    color: '#777',
-    borderColor: '#777'
-  },
-  primary: {
-    color: colors['Light Blue'],
-    borderColor: colors['Light Blue']
-  },
-  success: {
-    color: colors.Red,
-    borderColor: colors.Red,
-  },
-  info: {
-    color: "#5bc0de",
-    borderColor: "#5bc0de"
-  },
-  warning: {
-    color: "#f0ad4e",
-    borderColor: "#f0ad4e"
-  },
-  danger: {
-    color: "#d9534f",
-    borderColor: "#d9534f"
-  }
-};
+const labelStyles = [
+  ['default', '#777'],
+  ['primary', colors['Light Blue']],
+  ['success', colors.Red],
+  ['info', "#5bc0de"],
+  ['warning', "#f0ad4e"],
+  ['danger', "#d9534f"]
+];
+
+const getLabelColor = (style: string) =>
+  labelStyles.find(i => i[0] === style)[1];
+
+const LabelView = styled.View`
+  border-radius: 3px;
+  border: 1px solid;
+  padding: 3px 12px;
+  margin-bottom: ${spacing};
+  align-items: center;
+  justify-content: center;
+  border-color: ${props => getLabelColor(props.type)};
+`;
+
+const LabelText = styled.Text`
+  letter-spacing: 1px;
+  font-size: 10.5;
+  color: ${props => getLabelColor(props.type)};
+`;
 
 const Label = ({ children, type } : {
   children: Element<*>,
   type: 'default' | 'primary' | 'success' | 'info' | 'warning' | 'danger'
 }) =>
-  <View style={{ ...styles.label, ...styles[type] }}>
-    <Text style={{
-      color: styles[type].color,
-      letterSpacing: 1,
-      fontSize: 10.5
-    }}>
+  <LabelView type={type}>
+    <LabelText type={type}>
       {children}
-    </Text>
-  </View>;
+    </LabelText>
+  </LabelView>;
 
 Label.defaultProps = {
   type: 'default'
